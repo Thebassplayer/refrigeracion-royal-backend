fbdd376da4525d2a5e0beb113b2d59099f3d8b93
Roy Lopez <roylopezdev@gmail.com>
2023-06-10 16:46:39 -0300

    carousel schema update

--- schemas/carousel.js
+++ schemas/carousel.js
@@ -1,74 +1,60 @@
 export default {
   name: 'carousel',
-  type: 'document',
   title: 'Carousel',
+  type: 'document',
   fields: [
     {
       name: 'title',
-      type: 'string',
       title: 'Title',
+      type: 'string',
     },
     {
       name: 'images',
-      type: 'array',
       title: 'Images',
+      type: 'array',
       of: [
         {
           name: 'carouselImage',
-          type: 'object',
           title: 'Carousel Image',
+          type: 'object',
           fields: [
             {
               name: 'caption',
-              type: 'string',
               title: 'Caption',
+              type: 'string',
             },
             {
               name: 'imageFile',
-              type: 'image',
               title: 'Image File',
+              type: 'image',
               options: {
                 hotspot: true,
               },
-              fields: [
+            },
+            {
+              name: 'resolutions',
+              title: 'Resolutions',
+              type: 'array',
+              of: [
                 {
-                  name: 'resolutions',
-                  type: 'array',
-                  title: 'Resolutions',
-                  of: [
-                    {
-                      type: 'string',
-                      title: 'Resolution',
-                      options: {
-                        list: [
-                          {title: '640', value: '640'},
-                          {title: '768', value: '768'},
-                          {title: '1024', value: '1024'},
-                          {title: '1280', value: '1280'},
-                        ],
-                      },
-                    },
-                  ],
+                  type: 'string',
+                  options: {
+                    list: [
+                      {title: '640', value: '640w'},
+                      {title: '768', value: '768w'},
+                      {title: '1024', value: '1024w'},
+                      {title: '1280', value: '1280w'},
+                    ],
+                  },
                 },
               ],
-              preview: {
-                select: {
-                  imageUrl: 'asset.url',
-                  resolutions: 'resolutions',
-                },
-                prepare(selection) {
-                  const {imageUrl, resolutions} = selection
-                  const resolution = resolutions && resolutions.length > 0 ? resolutions[0] : null
-                  const url = imageUrl && resolution ? `${imageUrl}?w=${resolution}w` : imageUrl
-
-                  return {
-                    title: url,
-                    imageUrl: url,
-                  }
-                },
-              },
             },
           ],
+          preview: {
+            select: {
+              media: 'imageFile',
+            },
+          },
         },
       ],
     },
